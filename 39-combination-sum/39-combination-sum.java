4a4322dfc9695eb935d0bab3773a0aa498e8ab6b
Margi <39212098+margi212@users.noreply.github.com>
2022-05-24 21:38:36 +0530

    Time: 5 ms (61.05%), Space: 44.7 MB (57.02%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        generateComb(candidates,0,target,new ArrayList<Integer>(),ans);
+        
+        return ans;
+    }
+    
+    private void generateComb(int[] nums,int ci,int target,List<Integer> current,List<List<Integer>> ans){
+        
+        if(target == 0){
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+        
+        if(ci>=nums.length){
+            return;
+        }
+        
+        if(nums[ci]<=target){
+            current.add(nums[ci]);
+            generateComb(nums,ci,target-nums[ci],current,ans);
+            current.remove(current.size()-1);
+        }
+        
+        generateComb(nums,ci+1,target,current,ans);
+        return;       
+    }
+        
+}
