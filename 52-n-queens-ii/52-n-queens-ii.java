182ca28a99725d2c21c37147b12fe1f68fa0aba8
Margi <39212098+margi212@users.noreply.github.com>
2022-05-27 08:50:34 +0530

    Time: 4 ms (56.28%), Space: 41.6 MB (35.18%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,92 @@
+class Solution {
+    public int totalNQueens(int n) {
+     
+       // List<List<String>> answer = new ArrayList<>();
+        int[] answer = {0};
+        char[][] board = new char[n][n];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        
+        nQueens(board,0,n,answer);
+        return answer[0];
+    }
+    
+    private void nQueens(char[][] board,int currentRow,int n, int[] answer){
+        if(currentRow==n){
+            answer[0]+=1;
+            return;
+        }
+    
+        for(int currentCol = 0;currentCol<n;currentCol++){
+            if(isValid(board,currentRow,currentCol,n)){
+                board[currentRow][currentCol]='Q';
+                nQueens(board,currentRow+1,n,answer);
+                board[currentRow][currentCol]='.';
+            }   
+        }
+        
+        return;
+    }
+
+    private List<String> createAns(char[][] board,int n){
+        List<String> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            String cRow = "";
+            for(int j=0;j<n;j++){
+                cRow += board[i][j];
+            }
+            ans.add(cRow);
+        }
+        
+        return ans;
+    }
+
+   private boolean isValid(char[][] board,int currentRow,int currentCol,int n){
+    return isRowValid(board,currentRow,n) && isColValid(board,currentCol,n) && isGridValid(board,currentRow,currentCol,n);
+   }
+
+    private boolean isRowValid(char[][] board, int currentRow, int n) {
+        for(int currentCol = 0; currentCol<n; currentCol++)
+            if(board[currentRow][currentCol] == 'Q')
+                return false;
+        return true;
+    }
+    
+    private boolean isColValid(char[][] board, int currentCol, int n) {
+        for(int currentRow = 0; currentRow<n; currentRow++)
+            if(board[currentRow][currentCol] == 'Q')
+                return false;
+        return true;
+    }
+    
+    private boolean isGridValid(char[][] board,int currentRow, int currentCol, int n) {
+         
+        //leftUpDia
+        int x = currentRow;
+        int y = currentCol;
+        
+        while(x >= 0 && y >= 0){
+            if(board[x][y]=='Q')
+                return false;
+            x-=1;
+            y-=1;
+        }
+            
+        //rigthUp
+         x = currentRow;
+         y = currentCol;
+        
+        while(x >= 0 && y < n){
+            if(board[x][y]=='Q')
+                return false;
+            x-=1;
+            y+=1;
+        }
+        
+        return true;
+    }
+}
