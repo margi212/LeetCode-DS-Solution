da2ad0853a78eb075bd40a2d71513817bc1e2b1e
Margi <39212098+margi212@users.noreply.github.com>
2022-06-09 21:47:57 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+        for(int cVertex = 0;cVertex<V;cVertex++){
+            if(!visited[cVertex]){
+                if(isGetCycle(cVertex,adj,visited,-1)){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean isGetCycle(int cVertex, ArrayList<ArrayList<Integer>> adj,boolean[] visited,int cParent){
+        visited[cVertex] = true;
+        for(int cNeigh: adj.get(cVertex)){
+            if(visited[cNeigh] == false){
+                if(isGetCycle(cNeigh,adj,visited,cVertex)){
+                    return true;
+                }
+            }else if(cNeigh!=cParent)
+                return true;
+        }
+        
+        return false;
+    }
+}
