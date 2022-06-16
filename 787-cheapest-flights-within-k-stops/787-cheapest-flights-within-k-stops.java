b3933d0524747e8a9e320a7aba70f9e57d4d8a09
Margi <39212098+margi212@users.noreply.github.com>
2022-06-16 20:14:06 +0530

    Time: 8 ms (60.49%), Space: 46.5 MB (67.64%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -9,19 +9,19 @@ class Solution {
         costs[src] = 0;
         
         for(int i = 0; i <= k; i++){
-            int[] tempPrices = Arrays.copyOf(costs, n);
+            int[] tempCosts = Arrays.copyOf(costs, n);
             
             for(int[] edge:flights){
                 int u = edge[0];
                 int v = edge[1];
                 int w = edge[2];
                 
-                if(costs[u] != Integer.MAX_VALUE  && costs[u] + w < tempPrices[v]){
-                     tempPrices[v] = costs[u] + w;
-                 }
+                if(costs[u] != Integer.MAX_VALUE  && costs[u] + w < tempCosts[v]){
+                     tempCosts[v] = costs[u] + w;
+                }
             }
             
-            costs = tempPrices;
+            costs = tempCosts;
         }
         
         return costs[dest]==Integer.MAX_VALUE ? -1 : costs[dest];  
