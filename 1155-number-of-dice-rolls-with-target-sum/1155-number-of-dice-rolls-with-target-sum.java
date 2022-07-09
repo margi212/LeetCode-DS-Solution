788b990862c11a078cf3deba815f656c185d04db
Margi <39212098+margi212@users.noreply.github.com>
2022-07-10 01:14:59 +0530

    Time: 596 ms (5.02%), Space: 82.9 MB (5.16%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numRollsToTarget(int n, int f, int target) {
+        return countDice(n,f,target,new HashMap<String,Integer>());
+    }
+    
+    private int countDice(int dice,int face,int targetSum,HashMap<String,Integer> map){
+        if(targetSum == 0 && dice == 0)
+            return 1;
+        
+        if(dice==0 || targetSum <= 0)
+            return 0;
+        
+        String key = dice+"-"+face+"-"+targetSum;
+        
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+        
+        int ways = 0;
+        int MOD = 1000000007;
+        //(a+b)%c = (a%c + b%c) %c
+        
+        for(int i = 1; i <= face; i++){
+            int temp = countDice(dice-1,face,targetSum-i,map) % MOD;
+            ways = ways % MOD;
+            ways = (ways + temp) % MOD;
+        }
+       
+        map.put(key,ways);
+        return ways;
+    }
+}
