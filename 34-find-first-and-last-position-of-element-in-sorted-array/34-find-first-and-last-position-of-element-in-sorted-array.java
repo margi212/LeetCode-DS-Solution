2ac1bd79c49c0eee0259fb68c6de36678b7bce33
Margi <39212098+margi212@users.noreply.github.com>
2022-07-14 16:12:31 +0530

    Time: 0 ms (100.00%), Space: 47.4 MB (39.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,59 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = new int[2];
         
-        result[0] = getFirstPosition(nums,target);
-        result[1] = getLastPosition(nums,target);
+        if(nums.length < 1)
+            return new int[]{-1, -1};
         
-        return result;
+        int[] answer = {-1, -1};
+            
+        answer[0] = findFirstPosition(nums, target);
+        answer[1] = findLastPosition(nums, target);
+        
+        return answer;
     }
     
-    private int getFirstPosition(int[] nums, int target){
-        int start = 0;
-        int end = nums.length - 1;
+    private int findFirstPosition(int[] nums, int target){
+        int answerIndex = -1;
         
-        int answerIndex = -1; 
-        while(start<=end){
-            int mid = start + ((end-start)/2);
+        int start = 0;
+        int end = nums.length-1;
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
             
+            //comparison & decision both
             if(nums[mid] == target){
                 answerIndex = mid;
                 end = mid-1;
-            }else if(nums[mid] < target){
-                start = mid + 1;
+            }//decision
+            else if(nums[mid] > target){
+                end = mid-1;
             }else{
-                end = mid - 1;
+                start = mid+1;
             }
         }
         
         return answerIndex;
     }
     
-     private int getLastPosition(int[] nums, int target){
-        int start = 0;
-        int end = nums.length - 1;
+    private int findLastPosition(int[] nums, int target){
+        int answerIndex = -1;
         
-        int answerIndex = -1; 
-        while(start<=end){
-            int mid = start + ((end-start)/2);
+        int start = 0;
+        int end = nums.length-1;
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
             
+            //comparison & decision both
             if(nums[mid] == target){
                 answerIndex = mid;
-                start = mid + 1;
-            }else if(nums[mid] < target){
-                start = mid + 1;
-            }else {
-                end = mid - 1;
+                start = mid+1;
+            }//decision
+            else if(nums[mid] > target){
+                end = mid-1;
+            }else{
+                start = mid+1;
             }
         }
         
