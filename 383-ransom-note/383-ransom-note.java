ed69e2bcce7345b28ec4ae23086edca161e7d516
Margi <39212098+margi212@users.noreply.github.com>
2022-08-25 22:25:43 +0530

    Time: 15 ms (50.98%), Space: 42.3 MB (98.21%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+         
+        HashMap<Character,Integer> mg = new HashMap<>();
+        for(Character ch:magazine.toCharArray()){
+            if(mg.containsKey(ch)){
+                mg.put(ch,mg.get(ch)+1);
+            }else{
+                mg.put(ch,1);
+            }
+        }
+        
+        HashMap<Character,Integer> rn = new HashMap<>();
+        for(Character ch:ransomNote.toCharArray()){
+            if(mg.containsKey(ch)){
+                if(mg.get(ch)<=0)
+                    return false;
+                else
+                    mg.put(ch,mg.get(ch)-1);
+            }else{
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
