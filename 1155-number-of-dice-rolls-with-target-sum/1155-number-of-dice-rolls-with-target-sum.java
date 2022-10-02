450a476b4c948e64157e7f786871d2aed68830a7
Margi <39212098+margi212@users.noreply.github.com>
2022-10-02 15:09:08 +0530

    Time: 653 ms (5.05%), Space: 82.4 MB (5.12%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,32 +1,31 @@
 class Solution {
     public int numRollsToTarget(int n, int f, int target) {
-        return countDice(n,f,target,new HashMap<String,Integer>());
+        return countDice(n,f,target,0,new HashMap<String,Integer>());
     }
     
-    private int countDice(int dice,int face,int targetSum,HashMap<String,Integer> map){
-        if(targetSum == 0 && dice == 0)
+    private int countDice(int dice,int faces, int targetSum,int ci, HashMap<String,Integer> map){
+        if(targetSum == 0 && dice==0)
             return 1;
         
-        if(dice==0 || targetSum <= 0)
+        if(targetSum<0 || dice<=0)
             return 0;
+        
+        int totalWays = 0;
         
         String key = dice+"-"+targetSum;
         
-        if(map.containsKey(key)){
+        if(map.containsKey(key))
             return map.get(key);
-        }
         
-        int ways = 0;
-        int MOD = 1000000007;
-        //(a+b)%c = (a%c + b%c) %c
-        
-        for(int i = 1; i <= face; i++){
-            int temp = countDice(dice-1,face,targetSum-i,map) % MOD;
-            ways = ways % MOD;
-            ways = (ways + temp) % MOD;
+        //(a+b)%c = (a%/c + b%c) %c
+        int mod = 1000000007;
+        for(int i=1;i<=faces;i++){
+            int temp = countDice(dice-1,faces,targetSum-i,i,map)%mod;
+            totalWays = totalWays % mod;
+            totalWays = (totalWays + temp ) % mod;
         }
-       
-        map.put(key,ways);
-        return ways;
+                
+        map.put(key,totalWays);
+        return totalWays;
     }
 }
