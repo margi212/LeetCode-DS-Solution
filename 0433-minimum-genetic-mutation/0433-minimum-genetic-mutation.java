a22de947fb5d3287c62c7b5f4e3e4e72e95c5690
Margi <39212098+margi212@users.noreply.github.com>
2022-11-02 17:39:40 +0530

    Time: 1 ms (92.51%), Space: 40.4 MB (86.27%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minMutation(String start, String end, String[] bank) {
+        Character[] choices = {'A','C','G','T'};
+        
+        Set<String> bankSet = new HashSet<>();
+        Set<String> seen = new HashSet<>();
+        
+        for(String b:bank){
+            bankSet.add(b);
+        }
+        
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(start);
+        seen.add(start);
+        
+        int level = 0;
+        char[] currArr;
+        while(!queue.isEmpty()){
+          int size=queue.size();
+
+          while(size-->0){
+            String curr=queue.poll();
+            if(curr.equals(end)) return level;
+            currArr=curr.toCharArray();
+
+            for(int i=0;i<currArr.length;i++){
+                char old=currArr[i];
+           
+            for(char c:choices){
+                currArr[i]=c;
+                String newStr=new String(currArr);
+                if(!seen.contains(newStr)&& bankSet.contains(newStr)){
+                    queue.offer(newStr);
+                    seen.add(newStr);
+
+                }
+            }
+            currArr[i]=old;
+             }
+          }
+          level++;
+        }
+        return -1;
+        
+        
+       // return level;
+    }
+}
