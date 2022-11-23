81aac065b39572fe6fed9048882b99ea11b780c8
Margi <39212098+margi212@users.noreply.github.com>
2022-11-23 13:21:12 +0530

    Time: 93 ms (25.57%), Space: 91.4 MB (43.39%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,41 +1,41 @@
 class Solution {
     public boolean checkValid(int[][] matrix) {
-//         int n = matrix[0].length;
+        int n = matrix.length;
         
-//         for(int i=0;i<n;i++){
-//             HashMap<Integer,Integer> map1 = new HashMap<>();
-//             HashMap<Integer,Integer> map2 = new HashMap<>();
+        for(int i=0;i<n;i++){
+            HashMap<Integer,Integer> map1 = new HashMap<>();
+            HashMap<Integer,Integer> map2 = new HashMap<>();
          
-//             for(int j=0;j<n;j++){
-//                  if(!map1.containsKey(matrix[i][j])){
-//                      map1.put(matrix[i][j],1);
-//                  }else{
-//                      return false;
-//                  }
+            for(int j=0;j<n;j++){
+                 if(!map1.containsKey(matrix[i][j])){
+                     map1.put(matrix[i][j],1);
+                 }else{
+                     return false;
+                 }
                 
-//                  if(!map2.containsKey(matrix[j][i])){
-//                      map2.put(matrix[j][i],1);
-//                  }else{
-//                      return false;
-//                  }
-//             }
-//         }
-        
-//         return true;
-        
-            int n = matrix.length ;
-            Set<String> seen = new HashSet<>();
-            for(int i =0 ;i < n ; i++){
-                for(int j = 0 ;j < n ; j++){
-                    if(!seen.add(matrix[i][j]+"row"+i))
-                        return false;
-                    
-                    if(!seen.add(matrix[i][j]+"col"+j))
-                        return false;
-                  
-                }
+                 if(!map2.containsKey(matrix[j][i])){
+                     map2.put(matrix[j][i],1);
+                 }else{
+                     return false;
+                 }
             }
+        }
+        
+        return true;
+        
+//             int n = matrix.length ;
+//             Set<String> seen = new HashSet<>();
+//             for(int i =0 ;i < n ; i++){
+//                 for(int j = 0 ;j < n ; j++){
+//                     if(!seen.add(matrix[i][j]+"row"+i))
+//                         return false;
+                    
+//                     if(!seen.add(matrix[i][j]+"col"+j))
+//                         return false;
+                  
+//                 }
+//             }
             
-            return true;
+//             return true;
     }
 }
