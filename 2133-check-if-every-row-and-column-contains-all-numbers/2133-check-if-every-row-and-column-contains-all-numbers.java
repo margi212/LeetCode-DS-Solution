dfdf1372a317b86695aa6842231f3f38376c9e04
Margi <39212098+margi212@users.noreply.github.com>
2022-11-23 13:31:30 +0530

    Time: 49 ms (62.36%), Space: 43.2 MB (94.92%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -3,24 +3,46 @@ class Solution {
         int n = matrix.length;
         
         for(int i=0;i<n;i++){
-            HashMap<Integer,Integer> map1 = new HashMap<>();
-            HashMap<Integer,Integer> map2 = new HashMap<>();
-         
+            HashMap<Integer,Boolean> map1 = new HashMap<>();
+            HashMap<Integer,Boolean> map2 = new HashMap<>();
+           
             for(int j=0;j<n;j++){
+                
                  if(!map1.containsKey(matrix[i][j])){
-                     map1.put(matrix[i][j],1);
+                     map1.put(matrix[i][j],true);
                  }else{
-                     return false;
+                     map1.put(matrix[i][j],false);
+                     //return false;
                  }
                 
                  if(!map2.containsKey(matrix[j][i])){
-                     map2.put(matrix[j][i],1);
+                     map2.put(matrix[j][i],true);
                  }else{
-                     return false;
+                     map2.put(matrix[j][i],false);
+                     //return false;
                  }
             }
-        }
+            
+            
         
+        
+   
+        for (Map.Entry<Integer,Boolean> entry : map1.entrySet()) {
+                Integer key = entry.getKey();
+                Boolean value = entry.getValue();
+                if(!value)
+                    return false;
+                
+            }
+        
+        for (Map.Entry<Integer,Boolean> entry : map2.entrySet()) {
+                Integer key = entry.getKey();
+                Boolean value = entry.getValue();
+                if(!value)
+                    return false;
+                
+            }
+        }
         return true;
         
 //             int n = matrix.length ;
