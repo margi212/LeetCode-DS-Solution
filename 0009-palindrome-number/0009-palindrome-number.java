75d62422ccc442c76031d0f683df7cb7f1a57555
Margi <39212098+margi212@users.noreply.github.com>
2022-11-24 10:37:26 +0530

    Time: 16 ms (52.74%), Space: 44.1 MB (76.07%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,23 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = Integer.toString(x);
+//         String s = Integer.toString(x);
         
-        int count = s.length()/2;
-        for (int i=0; i< count ; i++){
-            if(Character.compare(s.charAt(i), s.charAt(s.length()-1-i) )!=0 ){
-                return false;
-            }
-        }
+//         int count = s.length()/2;
+//         for (int i=0; i< count ; i++){
+//             if(Character.compare(s.charAt(i), s.charAt(s.length()-1-i) )!=0 ){
+//                 return false;
+//             }
+//         }
 
-        return true;
+//         return true;
+        
+        int temp = x;
+        int reverse = 0;
+        while(temp>0) {
+            reverse = temp%10 + reverse*10;
+            temp = temp/10;
+        }
+        return reverse==x?true:false;
+
     }
 }
