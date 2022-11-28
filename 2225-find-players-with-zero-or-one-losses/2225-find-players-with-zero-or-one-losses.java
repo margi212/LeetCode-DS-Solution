13f340b3add21a698e85dd9e61f4cb8f30cd44d2
Margi <39212098+margi212@users.noreply.github.com>
2022-11-28 17:21:15 +0530

    Time: 353 ms (21.22%), Space: 152.8 MB (50.68%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -4,10 +4,11 @@ class Solution {
         
         int m = matches.length;
         
-        HashMap<Integer,Integer> map = new HashMap<>();
+        //used tree map because order is matter
+        //Key:PlayerNumber Value:How many time loses
+        TreeMap<Integer,Integer> map = new TreeMap<>();
         
         for(int w =0;w<m;w++){
-           // map.put(Long.valueOf(matches[w][0]), 0);
              map.put(matches[w][0], 0);
         }
         
@@ -20,17 +21,11 @@ class Solution {
         for(Map.Entry<Integer,Integer> mp:map.entrySet()){
             //System.out.println(mp.getKey()+":"+mp.getValue());
             if(mp.getValue()==0){
-                //zeroLoss.add((int) (long)mp.getKey());
                 zeroLoss.add(mp.getKey());
             }else if(mp.getValue()==1){
-               // oneLoss.add((int) (long)mp.getKey());
                  oneLoss.add(mp.getKey());
             }
-            
         }
-        
-        Collections.sort(zeroLoss);
-        Collections.sort(oneLoss);
         
         ans.add(zeroLoss);
         ans.add(oneLoss);
