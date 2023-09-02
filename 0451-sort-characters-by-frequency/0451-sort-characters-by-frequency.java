5d2fe78dfaf09dafdd0ca133de51739b4c05d931
Margi <39212098+margi212@users.noreply.github.com>
2023-09-02 06:19:12 +0530

    Time: 387 ms (17.23%), Space: 44.8 MB (37.55%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,20 +1,36 @@
 class Solution {
     public String frequencySort(String s) {
-        StringBuilder res = new StringBuilder();
-        HashMap<Character,Integer> freqMap = new HashMap<>();
-        
-        for(Character ch:s.toCharArray()){
-            freqMap.put(ch,freqMap.getOrDefault(ch,0)+1);
+        HashMap<Character,Integer> map = new HashMap<>();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char cCh = s.charAt(i);
+            map.put(cCh,map.getOrDefault(cCh,0)+1);   
         }
         
-        freqMap.entrySet().stream()
-        .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-        .forEach(x -> {
-            for(int i=0;i<x.getValue();i++){
-                res.append(x.getKey());
-            }
-        });
+        //sort map according to value
+        map = sortByValue(map);
         
-        return res.toString();
+        // System.out.println(Arrays.asList(map));
+        
+        String ans = "";
+        for(Map.Entry<Character,Integer> entry: map.entrySet()){
+            int val = entry.getValue();
+            for(int i=0;i<val;i++)
+                ans= ans+entry.getKey();
+        }
+        
+        return ans;
+    }
+    
+     public static HashMap<Character, Integer> sortByValue(HashMap<Character, Integer> hm) {
+        HashMap<Character, Integer> temp = hm.entrySet().stream()
+                                            .sorted((i1, i2)
+                                                      -> i2.getValue().compareTo(i1.getValue()))
+                                            .collect(Collectors.toMap(
+                                                Map.Entry::getKey,
+                                                Map.Entry::getValue,
+                                                (e1, e2) -> e1, LinkedHashMap::new));
+ 
+        return temp;
     }
 }
