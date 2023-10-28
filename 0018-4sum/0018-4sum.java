8cf7973b7b6703cbc1e032e43a6d53c0898ee440
Margi <39212098+margi212@users.noreply.github.com>
2023-10-28 19:31:00 +0530

    Time: 60 ms (25.94%), Space: 44.8 MB (8.05%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,40 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        
-        Set<List<Integer>> res = new HashSet<>();
-        
         int n = nums.length;
-      
+        Set<List<Integer>> ans = new HashSet<>();
+
         for(int i=0;i<n-3;i++){
             for(int j=i+1;j<n-2;j++){
-                int start = j+1;
-                int end = nums.length-1;
                 
-                while(end>start) {
-                    long sum = (long)(nums[i]+nums[j])+(long)(nums[start]+nums[end]);
+                int k = j+1;
+                int l = n-1;
+
+                while(l>k){
+                    long sum = (long)(nums[i]+nums[j])+(long)(nums[k]+nums[l]);
                     
-                    if(sum == target){
-                        res.add(Arrays.asList(nums[i], nums[j], nums[start++], nums[end--]));
-                    }else if(sum>target) {
-                        end--;
-                    }else {
-                        start++;
+                    if(sum==target){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);    
+                        ans.add(temp);  
+                        k++;
+                        l--;          
+                    }else if(sum>target){
+                        l--;
+                    }else{
+                        k++;
                     }
                 }
             }
         }
-        
-        return new ArrayList<>(res);	
+
+        List<List<Integer>> ans1 = new ArrayList<>();
+        for(List<Integer> ls: ans){
+            ans1.add(ls);
+        }
+        return ans1;
     }
 }
